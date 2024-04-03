2bed26ed05ccdcfae4e041a7001f8950c87058f3
David Chavez <depch47@gmail.com>
2024-04-02 20:56:39 -0500

    Add schema validation middleware to routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
 import { createUserHandler } from "../controller/user.controller"
+import { createUserSchema } from "../schema/user.schema"
+import validate from "../middleware/validateResource"
 
 const router = Router()
 
-router.post("/users", createUserHandler)
+router.post("/users", validate(createUserSchema), createUserHandler)
 
 
 export default router
---
+++ src/schema/user.schema.ts
@@ -0,0 +1,23 @@
+import z from "zod"
+
+export const createUserSchema = z.object({
+    body: z.object({
+        name: z.string({
+            required_error: "Name is required"
+        }).min(2, "Name must be at least 2 characters long"),
+        email: z.string({
+            required_error: "Email is required"
+        }).email("A valid email address is required"),
+        password: z.string({
+            required_error: "Password is required"
+        }).min(6, "Password must be at least 6 characters long"),
+        passwordConfirmation: z.string({
+            required_error: "PasswordConfirmation is required"
+        }),
+    }).refine((data) => data.password === data.passwordConfirmation, {
+        message: "Password do not match",
+        path: ["passwordConfirmation"],
+    }),
+})
+
+export type createUserInput = z.TypeOf<typeof createUserSchema>
